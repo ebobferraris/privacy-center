8cb840ff4d023408f2985e40d5b448ffc9af1419
ebobferraris <62755347+ebobferraris@users.noreply.github.com>
2025-09-08 21:05:20 +0200

    fix variable declaration

--- src/js/main.js
+++ src/js/main.js
@@ -186,7 +186,6 @@ function loadVersionContent(version) {
   const versionSelect = document.getElementById('version-select');
   const latestVersion = versionSelect ? versionSelect.getAttribute('data-latest-version') : null;
   
-  const currentPath = window.location.pathname;
   const basePath = currentPath.startsWith('/privacy-center') ? '/privacy-center' : '';
   
   let newUrl;
